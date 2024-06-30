58781c67f2e3cbc2d4e5023df458c9249a304213
martiya99 <37908735+martiya99@users.noreply.github.com>
2024-07-01 00:41:07 +0300

    Update module_4_1.py

--- module_4_1.py
+++ module_4_1.py
@@ -1,16 +1,10 @@
-import fake_math as fm
-import true_math as tm
-
-
-
-fake_divide = fm.divide
-true_divide = tm.divide
+from fake_math import divide as fake_divide
+from true_math import divide as true_divide
 
 result1 = fake_divide(69, 3)
 result2 = fake_divide(3, 0)
 result3 = true_divide(49, 7)
 result4 = true_divide(15, 0)
-
 print(result1)
 print(result2)
 print(result3)
