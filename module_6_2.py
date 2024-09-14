e6bac69084c8d69f1ea73ca93e9878399f536b6c
martiya99 <37908735+martiya99@users.noreply.github.com>
2024-09-14 14:50:11 +0300

    Create module_6_2.py

---
+++ module_6_2.py
@@ -0,0 +1,39 @@
+class Vehicle:
+    __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
+    def __init__(self, owner: str, model: str, engine_power: int,color: str):
+        self.owner = owner
+        self.__model = model
+        self.__engine_power = engine_power
+        self.__color = color
+
+    def get_model(self):
+        return f'Модель: {self.__model}'
+
+    def get_horsepower(self):
+        return f'Мощность двигателя: {self.__engine_power} л.с.'
+
+    def get_color(self):
+        return f'Цвет: {self.__color}'
+
+    def print_info(self):
+        print(f'Модель: {self.__model}\nМощность двигателя: {self.__engine_power}'
+              f'\nЦвет: {self.__color}\nВладелец: {self.owner}')
+
+    def set_color(self, new_color: str):
+        if new_color.lower() in self.__COLOR_VARIANTS:
+            self.__color = new_color
+        else:
+            print(f'Нельзя сменить цвет на: {new_color}')
+
+class Sedan(Vehicle):
+    __PASSENGERS_LIMIT = 5 
+
+vehicle1 = Sedan('Fedos', 'Toyota Mark II', 500, 'blue')
+
+vehicle1.print_info()
+
+vehicle1.set_color('Pink')
+vehicle1.set_color('BLACK')
+vehicle1.owner = 'Vasyok'
+
+vehicle1.print_info()
